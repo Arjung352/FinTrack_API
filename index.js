3caa29b1bc2165ca49d79be7346de8ca1b1babf9
Arjun gupta <rachnag352@gmail.com>
2026-04-03 23:37:39 +0530

    successfully tested the database connectivity

--- index.js
+++ index.js
@@ -6,22 +6,51 @@ const prisma = require("./src/lib/prisma");
 
 const PORT = process.env.PORT || 3000;
 
+// check database connection before starting the server
 async function testDatabaseConnection() {
   try {
-    await prisma.user.findMany({ take: 1 });
-    console.log("✅ Database connection successful");
+    const user = await prisma.user.findMany({ take: 1 });
+    console.log("Database connection successful");
   } catch (error) {
     console.error("❌ Database connection failed:", error.message);
     process.exit(1);
   }
 }
-app.get("/", (req, res) => {
-  res.send("Welcome to the FinTrack API! Use /api/v1 for endpoints.");
+// home route
+app.get("/", async (req, res) => {
+  try {
+    const user = await prisma.user.findMany();
+    res.status(200).json({
+      data: user,
+      response: "Data fetched successfully from the database",
+    });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
 });
+
+// sample user input
+async function createSampleUser() {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: "Arjun",
+        email: "arjung352@gmail.com",
+        passwordHash: "password",
+        role: "ADMIN",
+        status: "ACTIVE",
+      },
+    });
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// server start function
 async function startServer() {
   try {
     await testDatabaseConnection();
-
+    // createSampleUser();
     app.listen(PORT, () => {
       console.log(`🚀 FinTrack API server running on port ${PORT}`);
     });
